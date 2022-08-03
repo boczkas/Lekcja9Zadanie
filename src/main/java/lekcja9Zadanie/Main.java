226b4c3359de980a6b109a05cf93b5bd37554eac
Przemek <przemyslawjakubowski@gmail.com>
2022-08-03 14:17:00 +0200

    task

---
+++ src/main/java/lekcja9Zadanie/Main.java
@@ -0,0 +1,27 @@
+package lekcja9Zadanie;
+
+public class Main {
+    public static void main(String[] args) {
+//        1. Klasa Film
+//              klasa ma posiadać pola:
+//              String nazwa
+//              int dlugosc
+
+//              klasa ma posiadać metodę:
+//              wyswietlFilm();
+//
+//              w której otrzymamy informację jaki film będzie aktualnie wyświetlany
+//
+//        2. Klasa SalaKinowa
+//              klasa ma posiadać pola:
+//              Film film
+//              int iloscMiejsc
+//
+//              klasa ma posiadać metodę:
+//              uruchom();
+//
+//              w której wypiszemy jak długo będzie wyświetlany film
+
+
+    }
+}
